c33e20fe7ecff574e41145e5c26fc41fb9c8e8d9
Quasimorphism <bak575755@gmail.com>
2026-08-04 14:13:34 +0900

    refactor: 인증 관련 page controller들을 web config로 이동

--- src/main/java/site/omagotchi/frontend/global/config/WebConfig.java
+++ src/main/java/site/omagotchi/frontend/global/config/WebConfig.java
@@ -9,9 +9,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        // Server Model 없는 정적 Thymeleaf Page
         registry.addViewController("/")
                 .setViewName("index");
         registry.addViewController("/index")
                 .setViewName("index");
+        registry.addViewController("/login")
+                .setViewName("login");
+        registry.addViewController("/password-change")
+                .setViewName("passwordChange");
+        registry.addViewController("/register")
+                .setViewName("register");
+        registry.addViewController("/username")
+                .setViewName("username");
     }
 }
