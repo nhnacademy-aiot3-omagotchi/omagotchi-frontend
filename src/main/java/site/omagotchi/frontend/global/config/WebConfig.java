fe8ec493be28d87ba64dcc6c30ce44e897f50e98
Quasimorphism <bak575755@gmail.com>
2026-08-04 22:15:50 +0900

    refactor: 정적 page controller를 web config로 통합

--- src/main/java/site/omagotchi/frontend/global/config/WebConfig.java
+++ src/main/java/site/omagotchi/frontend/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Server Model 없는 정적 Thymeleaf Page
+        // 시작·회원 인증 Page
         registry.addViewController("/")
                 .setViewName("index");
         registry.addViewController("/index")
@@ -22,5 +22,35 @@ public class WebConfig implements WebMvcConfigurer {
                 .setViewName("register");
         registry.addViewController("/username")
                 .setViewName("username");
+
+        // 사용자 기능 Page
+        registry.addViewController("/character-selector")
+                .setViewName("characterSelector");
+        registry.addViewController("/check-in")
+                .setViewName("check-in");
+        registry.addViewController("/progress")
+                .setViewName("progress");
+        registry.addViewController("/personal")
+                .setViewName("personal");
+        registry.addViewController("/cohort")
+                .setViewName("cohort");
+        registry.addViewController("/write")
+                .setViewName("write");
+        registry.addViewController("/settings")
+                .setViewName("settings");
+        registry.addViewController("/help")
+                .setViewName("help");
+        registry.addViewController("/home")
+                .setViewName("home");
+        registry.addViewController("/space")
+                .setViewName("space");
+
+        // 관리자 Page
+        registry.addViewController("/manager-login")
+                .setViewName("managerLogin");
+        registry.addViewController("/manager-register")
+                .setViewName("managerRegister");
+        registry.addViewController("/manager-dashboard")
+                .setViewName("managerDashboard");
     }
 }
