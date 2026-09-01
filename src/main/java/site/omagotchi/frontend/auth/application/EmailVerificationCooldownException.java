5b604ee08dfdd77c468efcc815fc52058f562b4e
yjKang227 <hellcodedev@gmail.com>
2026-09-01 13:49:14 +0900

    refactor: retry-After 메타데이터를 공통 계약으로 분리

--- src/main/java/site/omagotchi/frontend/auth/application/EmailVerificationCooldownException.java
+++ src/main/java/site/omagotchi/frontend/auth/application/EmailVerificationCooldownException.java
@@ -1,21 +1,26 @@
 package site.omagotchi.frontend.auth.application;
 
 import site.omagotchi.frontend.global.exception.BusinessException;
+import site.omagotchi.frontend.global.exception.RetryAfterMetadata;
+import site.omagotchi.frontend.global.exception.RetryAfterSeconds;
 
 // Identity가 확정한 OTP 재요청 대기 시간을 Browser 응답까지 전달
-public class EmailVerificationCooldownException extends BusinessException {
+public class EmailVerificationCooldownException
+        extends BusinessException
+        implements RetryAfterMetadata {
 
-    private final long retryAfterSeconds;
+    private final RetryAfterSeconds retryAfter;
 
     public EmailVerificationCooldownException(
             long retryAfterSeconds,
             Throwable cause
     ) {
         super(AuthErrorCode.EMAIL_VERIFICATION_COOLDOWN_ACTIVE, cause);
-        this.retryAfterSeconds = retryAfterSeconds;
+        this.retryAfter = new RetryAfterSeconds(retryAfterSeconds);
     }
 
-    public long retryAfterSeconds() {
-        return retryAfterSeconds;
+    @Override
+    public RetryAfterSeconds retryAfter() {
+        return retryAfter;
     }
 }
